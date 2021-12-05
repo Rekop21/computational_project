bf599ea20e04c5170c20581d7347f1ee7ba30872
Rekop21 <Reko.pentti@gmail.com>
2021-12-05 22:59:18 +0200

    1D and 3D cases of voter_model

--- voter_model1D.py
+++ voter_model1D.py
@@ -4,48 +4,42 @@ import random
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-#test
-#Initialize the matrix with size and density given as arguments
-def initialize(size, density):
-    num_of_minuses = round((1-density) * size**2)
-    places = random.sample(range(size**2), int(num_of_minuses) )
 
-    matrix = ones(shape=(size,size))
-    
-    place = 0
+def initialize(size, density):
+    num_of_minuses = round((1-density) * size)
+    places = random.sample(range(size), int(num_of_minuses) )
+    matrix = ones(size)
     for i in range(size):
-        for j in range(size):
-            if place in places:
-                matrix[i][j] = -1
-            place += 1
+        if i in places:
+            matrix[i] = -1
 
     return matrix
 
 
-#Choose a random voter and adopt an value of a random voter.
+#Choose a random voter and adopt an value of a random voter. 1d case
 def adopt_value(matrix, n):
-    place =  (random.choice(range(n)), random.choice(range(n)))
-    directions = [[-1,0], [0,1], [1, 0], [0, -1]]
+    place =  random.choice(range(n))
+    directions = [1, -1]
 
     def helper(place, poss_directions):
         dir = random.choice(poss_directions)        
-        neighbour = (place[0]-dir[0], place[1]-dir[1])
-
-        if neighbour[0] in [-1, n] or neighbour[1] in [-1, n]:
+        neighbour = place - dir
+        if neighbour in [-1, n]:
             poss_directions.remove(dir)
             return helper(place, poss_directions)
         else:
             return neighbour
     
     neig = helper(place, directions)
-    matrix[place[0]][place[1]] = matrix[neig[0]][neig[1]]
+    matrix[place] = matrix[neig]
     return matrix
+
     
 
 #Check if matrix is complete. Returns the sum of entries divided by the size. So it returns either 1 or -1 if completed. 
 def check_matrix(matrix, size):
     sum = matrix.sum() #sum over all elements
-    return sum/(size**2)
+    return sum/(size)
     
     
 # Call adopt_value function until the matrix is completed. 
@@ -55,9 +49,11 @@ def solve_matrix(size, density):
     while check_matrix(matrix, size) not in [-1, 1]:
         matrix = adopt_value(matrix, size)
         n += 1
+    
     return (n, check_matrix(matrix, size))
 
-#This function calculates probabilities as a function of density
+solve_matrix(4, 0.5)
+#
 def different_densitys():
     densitys = []
     durations = []
@@ -81,40 +77,35 @@ def different_densitys():
     print(densitys)
     print(probability)
     print(durations)
-    plt.plot(densitys, probability, color='r', label='sim')
-    plt.show()
 
-#This finds out the consensus time as a function of size
+
 def different_sizes():
     sizes = []
     durations = []
-    ratio = []
+    suhde = []
 
-    for s in range(13):
+    for s in range(20):
         n = s+2
         N = []
-        for i in range(100):
+        for i in range(600):
             N.append(solve_matrix(n, 0.7)[0])
         durations.append(sum(N)/len(N))
-        sizes.append(n**2)
+        sizes.append(n)
     
+    print(sizes)
+    print(durations)
+    
+    #This nlog is used to obtain how the consensustime scale. In the append() parts can put different functions.
     nlog = []
     for i in range(len(sizes)):
         size = sizes[i]
-        nlog.append(0.27*size**2*math.log(size))
-        ratio.append(durations[i]/(size**2*math.log(size)))
-
-    # Ratio is used to investigate if consensus time and chosen function increase with the same rate.
-
-    print(sizes)
-    print(durations)
-    print(ratio)
-    
+        nlog.append(0.14*size**3)
+        suhde.append(durations[i]/(size**3))
+    print(suhde)
 
     plt.plot(sizes, durations, color='r', label='sim')
     plt.plot(sizes, nlog, color='g', label='nlog')
     plt.show()
-
 
 
 #Run this function to obtain data on the probabilities for different densities
--- voter_model3D.py
+++ voter_model3D.py
@@ -6,58 +6,73 @@ from scipy.optimize import curve_fit
 
 #test
 #Initialize the matrix with size and density given as arguments
+
+
 def initialize(size, density):
-    num_of_minuses = round((1-density) * size**2)
-    places = random.sample(range(size**2), int(num_of_minuses) )
+    num_of_minuses = round((1-density) * size**3)
+    places = random.sample(range(size**3), int(num_of_minuses) )
 
-    matrix = ones(shape=(size,size))
-    
     place = 0
+    tensor = []
     for i in range(size):
+        matrix = []
         for j in range(size):
-            if place in places:
-                matrix[i][j] = -1
-            place += 1
+            array = []
+            for l in range(size):
+                if place in places:
+                    array.append(-1)
+                else: array.append(1)
+                place += 1
 
-    return matrix
+            matrix.append(array)
+        tensor.append(matrix)
+
+    return tensor
 
 
 #Choose a random voter and adopt an value of a random voter.
 def adopt_value(matrix, n):
-    place =  (random.choice(range(n)), random.choice(range(n)))
-    directions = [[-1,0], [0,1], [1, 0], [0, -1]]
+    place =  (random.choice(range(n)), random.choice(range(n)), random.choice(range(n)))
+    directions = [[-1,0, 0], [1,0, 0], [0, 1, 0], [0,-1, 0], [0, 0, 1], [0, 0, -1]]
 
     def helper(place, poss_directions):
         dir = random.choice(poss_directions)        
-        neighbour = (place[0]-dir[0], place[1]-dir[1])
+        neighbour = (place[0]-dir[0], place[1]-dir[1], place[2]- dir[2])
 
-        if neighbour[0] in [-1, n] or neighbour[1] in [-1, n]:
+        if neighbour[0] in [-1, n] or neighbour[1] in [-1, n] or neighbour[2] in [-1, n]:
             poss_directions.remove(dir)
             return helper(place, poss_directions)
         else:
             return neighbour
     
     neig = helper(place, directions)
-    matrix[place[0]][place[1]] = matrix[neig[0]][neig[1]]
+    matrix[place[0]][place[1]][place[2]] = matrix[neig[0]][neig[1]][neig[2]]
     return matrix
     
 
 #Check if matrix is complete. Returns the sum of entries divided by the size. So it returns either 1 or -1 if completed. 
 def check_matrix(matrix, size):
-    sum = matrix.sum() #sum over all elements
-    return sum/(size**2)
+    sum = 0
+    for i in range(size):
+        for j in range(size):
+            for l in range(size):
+                sum += matrix[i][j][l]
+
+    return sum/(size**3)
     
     
 # Call adopt_value function until the matrix is completed. 
 def solve_matrix(size, density):
     matrix = initialize(size, density)
+    
     n = 0
     while check_matrix(matrix, size) not in [-1, 1]:
         matrix = adopt_value(matrix, size)
         n += 1
+    
     return (n, check_matrix(matrix, size))
 
-#This function calculates probabilities as a function of density
+#
 def different_densitys():
     densitys = []
     durations = []
@@ -81,40 +96,35 @@ def different_densitys():
     print(densitys)
     print(probability)
     print(durations)
-    plt.plot(densitys, probability, color='r', label='sim')
-    plt.show()
 
-#This finds out the consensus time as a function of size
+
 def different_sizes():
     sizes = []
     durations = []
-    ratio = []
+    suhde = []
 
-    for s in range(13):
+    for s in range(6):
         n = s+2
         N = []
-        for i in range(100):
-            N.append(solve_matrix(n, 0.7)[0])
+        for i in range(50):
+            N.append(solve_matrix(n, 0.8)[0])
         durations.append(sum(N)/len(N))
-        sizes.append(n**2)
+        sizes.append(n**3)
     
+    print(sizes)
+    print(durations)
+    
+#This nlog is used to obtain how the consensustime scale. In the append() parts can put different functions.
     nlog = []
     for i in range(len(sizes)):
         size = sizes[i]
-        nlog.append(0.27*size**2*math.log(size))
-        ratio.append(durations[i]/(size**2*math.log(size)))
+        nlog.append(0.58*size**2)
+        suhde.append(durations[i]/(size**2))
 
-    # Ratio is used to investigate if consensus time and chosen function increase with the same rate.
-
-    print(sizes)
-    print(durations)
-    print(ratio)
-    
-
+    print(suhde)
     plt.plot(sizes, durations, color='r', label='sim')
     plt.plot(sizes, nlog, color='g', label='nlog')
     plt.show()
-
 
 
 #Run this function to obtain data on the probabilities for different densities
